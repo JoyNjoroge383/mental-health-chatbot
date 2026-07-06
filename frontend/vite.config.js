116375299a41d7afe2246929e3582d90157fcd82
JoyNjoroge383 <joy393415@gmail.com>
2026-07-06 14:30:44 +0300

    vite.config.js change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    open: true,
-    proxy: {
-      '/api': {
-        target: 'https://mental-health-chatbot-4-yh9c.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      open: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL, // use env here too
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
       }
     }
   }
